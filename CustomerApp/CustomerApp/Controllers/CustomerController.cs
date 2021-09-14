e39443434d1ec7c32e70531815f32713e0851b08
unknown <harold_meza_cantillo@hotmail.com>
2021-09-14 10:22:40 -0500

    Comment deleted

--- CustomerApp/CustomerApp/Controllers/CustomerController.cs
+++ CustomerApp/CustomerApp/Controllers/CustomerController.cs
@@ -13,11 +13,7 @@ namespace CustomerApp.Controllers
     [Route("api/[controller]")]
     public class CustomerController : ControllerBase
     {
-        /*private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };*/
-
+        
         private readonly ILogger<CustomerController> _logger;
         private ICustomerRepository _customerRepository;
 
@@ -27,30 +23,10 @@ namespace CustomerApp.Controllers
             _customerRepository = customerRepository;
         }
 
-        /*[HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }*/
 
         [HttpGet]
         public ActionResult<IEnumerable<Customer>> GetCustomers()
         {
-            /*var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();*/
             return Ok(_customerRepository.GetCustomers());
         }
 
@@ -72,7 +48,6 @@ namespace CustomerApp.Controllers
         [HttpPost("updateLoans")]
         public ActionResult<IEnumerable<Loan>> UpdateLoans([FromBody] Customer customer)
         {
-            var pochera = "tronitra";
             if (!ModelState.IsValid)
             {
                 return BadRequest();
